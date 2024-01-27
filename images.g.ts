2f954c3ff2a65f0162eb2a4d3c2a34c4c290b8f4
briezielke <157852377+briezielke@users.noreply.github.com>
2024-01-27 14:31:45 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "square tetroblock":return img`
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 1 1 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 1 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
